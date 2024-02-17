c9f6919424231f2bd9f2fd1532cd5fd7b63db06f
Ahmadziad121 <ahmadziad758@gmail.com>
2024-02-17 18:48:41 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+x=getElementById("result").value
 function clearScreen(){
     document.getElementById("result").value=""
 }
@@ -12,3 +13,77 @@ function calculate(){
     catch(err){
     alert("erorr")} 
 }
+function e(){
+    document.getElementById("result").value=2.71
+
+}
+function tan(){
+    document.getElementById("result").value=Math.tan(document.getElementById("result").value)
+
+}
+function sqrt(){
+    document.getElementById("result").value=Math.sqrt(document.getElementById("result").value)
+
+}
+function pi(){
+    document.getElementById("result").value=3.14
+}
+function cos(){
+    document.getElementById("result").value=Math.cos(document.getElementById("result").value)
+
+}
+
+function log(){
+    document.getElementById("result").value=Math.log(document.getElementById("result").value)
+
+}
+function pow(){
+    document.getElementById("result").value=Math.pow(document.getElementById("result").value,document.getElementById("result").value)
+
+}
+function Exp(){
+    document.getElementById("result").value=Math.exp(document.getElementById("result").value)
+
+}
+function Ans(){
+    document.getElementById("result").value +=  document.getElementById("result").value
+    document.getElementById("result").value= eval(document.getElementById("result").value).value
+
+}
+
+function inv(){
+    document.getElementById("result").value=Math.asin(document.getElementById("result").value)
+
+}
+
+
+function sin(){
+    document.getElementById("result").value=Math.sin(document.getElementById("result").value)
+
+}
+function ln(){
+    document.getElementById("result").value=Math.log2(document.getElementById("result").value)
+
+}
+function deg_to_rad(){
+    document.getElementById("result").value=Math.PI/180(document.getElementById("result").value)
+}
+function rad_to_deg(){
+    document.getElementById("result").value=Math.PI/180(document.getElementById("result").value)
+}
+function x(){
+    var i,num,f
+    f=1
+    num=document.getElementById("result").value
+    for(i=1;i<=num;i++)
+    {
+        f=f*i
+    }
+    i=i-1
+    document.getElementById("result").value=f
+}
+
+
+
+
+
